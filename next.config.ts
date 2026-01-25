da478aff30cdcce8481c9084b67651b46fe8bf17
Marcelin97 <lois_m@outlook.com>
2026-01-25 15:17:57 +0100

    fix: résolution des conflits entre Turbopack et Prisma. check config Tailwind

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "standalone",
+    serverExternalPackages: ["@prisma/client"], // Force Prisma à être traité comme un package externe
 };
 
 export default nextConfig;
